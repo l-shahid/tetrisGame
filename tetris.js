d7558f629077a3ac691acd9d05815c86b17f0f3a
Eliezer Shahid <lshahid@udel.edu>
2022-04-03 09:05:11 -0400

    Create tetris.js

---
+++ tetris.js
@@ -0,0 +1,11 @@
+/**
+====================================================================
+Simple Tetris Game
+
+04-02-2022
+
+This JavaScript file is for building and playing a simple tetris game. 
+The plan to to build a web embeddable tetris game for my portfolio website.
+
+====================================================================
+*/
